3302a4c7045372c747117f99c9d3af094cdd127b
Jonathan Salazar <jonyalex1047@gmail.com>
2026-07-04 22:24:54 -0500

    feat: migrate to uv, add uvicorn hot reload, implement users list

--- app/main.py
+++ app/main.py
@@ -4,3 +4,12 @@ from fastapi import FastAPI
 app = FastAPI()
 
 app.include_router(v1_router)
+
+if __name__ == "__main__":
+    # Run the applicatio using Uvicorn (An ASGI web server)
+    import uvicorn
+
+    # app.main refers to the file name (main.py) and app refers to the FastAPI instance created in that file.
+    # app is the FastAPI instance that we created earlier in the code.
+    # reload=True enables automatic reloading of the application when code changes are detected, which is useful during development.
+    uvicorn.run("app.main:app", host="127.0.0.1", port=8000, reload=True)
