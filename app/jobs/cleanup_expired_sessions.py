58be23dcef192d989e221c3a8207661c8ba47c99
Jonathan Salazar <jonyalex1047@gmail.com>
2026-07-23 22:50:13 -0500

    feat(jobs): refactor cleanup functions to use session context manager

--- app/jobs/cleanup_expired_sessions.py
+++ app/jobs/cleanup_expired_sessions.py
@@ -1,6 +1,10 @@
 from app.dependencies import RequiresSessionRepository
 from app.jobs.scheduler import daily
-
+from sqlalchemy.orm import Session
+from app.db.session import engine
+    
+    
 @daily(id='cleanup_sessions')
-async def cleanup_expired_sessions(session_repo: RequiresSessionRepository):
-    session_repo.delete_all_expired()
+async def cleanup_expired_sessions():
+    with Session(engine) as session_repo:
+        session_repo.delete_all_expired()
--- app/jobs/cleanup_expired_tokens.py
+++ app/jobs/cleanup_expired_tokens.py
@@ -1,6 +1,10 @@
 from app.dependencies import RequiresTokenRepository
 from app.jobs.scheduler import daily
+from sqlalchemy.orm import Session
+from app.db.session import engine
 
+    
 @daily(id='cleanup_tokens')
-async def cleanup_expired_tokens(token_repo: RequiresTokenRepository):
-    token_repo.delete_all_expired()
+async def cleanup_expired_tokens():
+    with Session(engine) as token_repo:
+        token_repo.delete_all_expired()
